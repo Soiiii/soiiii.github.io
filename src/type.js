d3f98b74e1a97286c40219b8ecc2f37fc6a83e21
Soiiii <cojomkr33@gmail.com>
2024-03-04 18:36:40 +0900

    JS Home style

---
+++ src/type.js
@@ -0,0 +1,14 @@
+"use strict";
+
+new TypeIt(".home__title--strong", {
+  loop: true,
+  speed: 80,
+})
+  //   .move(-25)
+  //   .type("Amazing ")
+  //   .move(null, { to: "END" })
+  //   .delete()
+  //   .type("Back-end Engineer")
+  //   .move(-3)
+  //   .delete(3)
+  .go();
